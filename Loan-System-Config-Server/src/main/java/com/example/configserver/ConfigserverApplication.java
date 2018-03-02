68afc83c48ceebf56eb10260384c8960aa0bf5ec
AllenXZH <xzhxzh82@gmail.com>
2018-03-02 11:13:03 -0600

    issur fix

--- Loan-System-Config-Server/src/main/java/com/example/configserver/ConfigserverApplication.java
+++ Loan-System-Config-Server/src/main/java/com/example/configserver/ConfigserverApplication.java
@@ -1,20 +1,16 @@
-package demo;
+package com.example.configserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableConfigServer
-public class ConfigServerApplication {
+@EnableDiscoveryClient
+public class ConfigserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(ConfigserverApplication.class, args);
 	}
-
 }
